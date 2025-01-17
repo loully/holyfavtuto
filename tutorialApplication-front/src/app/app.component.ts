8e547e797b5624eedef80646aa59e80ce37071ab
loully <loully@github.com>
2025-01-17 12:45:18 +0100

    Update Navbar to Angular Material and add logo

--- tutorialApplication-front/src/app/app.component.ts
+++ tutorialApplication-front/src/app/app.component.ts
@@ -3,17 +3,21 @@ import {RouterLink, RouterOutlet} from '@angular/router';
 import {TutorialListComponent} from "./components/tutorial-list/tutorial-list.component";
 import {TutorialService} from "./services/tutorial.service";
 import {Tutorial} from "./models/tutorial.model";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatListModule} from "@angular/material/list";
+import {MatButtonModule} from "@angular/material/button";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, MatToolbarModule, MatListModule, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 
 export class AppComponent implements OnInit {
   title = 'tutorialApplication-front';
+  logoPath = "../assets/img/logo-100x123px.png";
 
   constructor(public tutorialService:TutorialService) {
   }
