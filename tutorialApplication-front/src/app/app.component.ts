a2b63ccd9f879edf9b2c8adb53020a415cd8aff3
loully <loully@github.com>
2025-06-24 17:29:06 +0200

    [Home] Insert Add button

--- tutorialApplication-front/src/app/app.component.ts
+++ tutorialApplication-front/src/app/app.component.ts
@@ -4,11 +4,12 @@ import {TutorialService} from "./services/tutorial.service";
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatListModule} from "@angular/material/list";
 import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, MatToolbarModule, MatListModule, MatButtonModule],
+  imports: [RouterOutlet, RouterLink, MatToolbarModule, MatListModule, MatButtonModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
